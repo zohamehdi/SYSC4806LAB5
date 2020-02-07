9935d66defda9710065c7e400ad7f2e4a8aa7a9a
zohamehdi <zmehdiz_96@hotmail.com>
2020-02-07 16:14:47 -0500

    new

---
+++ src/test/java/TestingSpringWeb.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSpringWeb {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingSpringWeb.class, args);
+    }
+}
+
